0a266b19db5b14016e5e4c3692beef72d0aab6ae
ChristianBaciu <christian3110@outlook.it>
2024-02-05 23:13:55 +0100

    HTML email

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-
+            listaEmail: '',
+            
+            // listaEmail: [ ],
         }
     },
     methods:{
 
         generaEmail(){
-
             for (let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
                 console.log(email.data.response)
+                this.listaEmail = email.data.response
 
                 })
             }
