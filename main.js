c3c7f93d9afdd1c645b9721b415ba5184caef738
ChristianBaciu <christian3110@outlook.it>
2024-02-05 16:30:48 +0100

    prova con axios.get

--- main.js
+++ main.js
@@ -7,5 +7,13 @@ createApp({
     },
     methods:{
 
+        generaEmail(){
+            // Quando effettuiamo una chiamata HTTP e tutto funziona correttamente,
+            // otteniamo un'istruzione da inserire nel blocco THEN
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then ((risultato) => {
+                console.log(risultato)
+            })
+        }
+
     },
 }).mount('#app')
