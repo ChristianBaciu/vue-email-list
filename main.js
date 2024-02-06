0a820959a67c3cc5100d65dc01ae987e15c45196
ChristianBaciu <christian3110@outlook.it>
2024-02-06 15:40:08 +0100

    email

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            listaEmail: '',
-            
-            // listaEmail: [ ],
+            listaEmail: [ ],
         }
     },
     methods:{
@@ -12,11 +10,13 @@ createApp({
         generaEmail(){
             for (let i = 0; i < 10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((email) => {
-                console.log(email.data.response)
-                this.listaEmail = email.data.response
+                console.log(email.data)
+
+                this.listaEmail.push(email.data.response)
 
                 })
             }
+            console.log(this.listaEmail)
         },
     }
 }).mount('#app');
